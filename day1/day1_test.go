b690e33b477bad32a17e31ee5cbed037eab1eea8
ccrowe <ccrowe@unomaha.edu>
2020-12-15 07:01:10 -0800

    finished part 1 correctly

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -7,7 +7,7 @@ import (
 func TestExample(t *testing.T) {
 	filePath := "./testInput.txt"
 	pair := solveDay1(filePath)
-	want := Pair{299, 1721}
+	want := Pair{299, 1721, 514579}
 	if want != pair {
 		t.Fatalf(`Want = %v, expected %v`, want, pair)
 	}
--- day1/main.go
+++ day1/main.go
@@ -26,30 +26,33 @@ func convertToNumbers(toConvert []string) []int {
 }
 
 type Pair struct {
-    a, b int
+    a, b, multiplied int
 }
 
-func getMagicNumber(numbers []int) Pair {
+func getPair(numbers []int) Pair {
 	// note that numbers are sorted
 	for i, number1 := range numbers {
 		for j := i+1; j < len(numbers); j++ {
 			number2 := numbers[j]
 			if(number1 + number2 == 2020){
-				if(number1 * number2 == 514579){
-					return Pair{number1, number2}
-				}
+				return Pair{number1, number2, number1*number2}
 			}
 		}
 	}
 	panic("There is no solution")
 }
 
-func main(){
-	filePath := "./input.txt"
+func solveDay1(filePath string) Pair {
 	text := readFile(filePath)
 	lines := strings.Split(text,"\n")
 	numbers := convertToNumbers(lines)
 	sort.Ints(numbers) // in place
-	magicNumber := getMagicNumber(numbers)
-	fmt.Println(magicNumber)
+	pair := getPair(numbers)
+	return pair
+}
+
+func main(){
+	filePath := "./input.txt"
+	pair := solveDay1(filePath)
+	fmt.Println(pair)
 }
