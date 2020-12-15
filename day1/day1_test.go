808aa073d1fde98c31d381635723f31740c02100
ccrowe <ccrowe@unomaha.edu>
2020-12-15 06:57:14 -0800

    added test for day1, part 1

---
+++ day1/day1_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestExample(t *testing.T) {
+	filePath := "./testInput.txt"
+	pair := solveDay1(filePath)
+	want := Pair{299, 1721}
+	if want != pair {
+		t.Fatalf(`Want = %v, expected %v`, want, pair)
+	}
+}
